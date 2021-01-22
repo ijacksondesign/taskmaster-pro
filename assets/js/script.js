6d73720fd99dd4b37f74ed436a4d7fb7478be902
Ian Jackson <ijacksondesign@gmail.com>
2021-01-21 19:01:16 -0500

    UI improvements

--- assets/js/script.js
+++ assets/js/script.js
@@ -159,13 +159,18 @@ $(".card .list-group").sortable({
   scroll: false,
   tolerance: "pointer",
   activate: function(event) {
-    console.log("deactivate", this);
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
+  },
+  deactivate: function(event) {
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function(event) {
-    console.log("over", event.target);
+    $(event.target).addClass("dropover-active");
   },
   out: function(event) {
-    console.log("out", event.target);
+    $(event.target).removeClass("dropover-active");
   },
   update: function(event) {
     //array to store the task data
@@ -228,8 +233,10 @@ $("#trash").droppable({
     ui.draggable.remove();
   },
   over: function(event, ui) {
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function(event, ui) {
+    $(".bottom-trash").removeClass("bottom-trash-active");
   }
 });
 
@@ -247,7 +254,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -285,4 +292,8 @@ $("#remove-tasks").on("click", function() {
 // load tasks for the first time
 loadTasks();
 
-
+setInterval(function() {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, (1000 * 60) * 30);
